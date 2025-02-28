72a31031257369bddde44ee9da55f562d649c45c
Luuk van der Duim <luukvanderduim@gmail.com>
2025-03-01 00:07:59 +0100

    feat: Object accounting added.

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,78 @@ impl InterfaceCount {
     }
 }
 
+struct ObjectCount {
+    total: AtomicU32,
+    categories: [(&'static str, AtomicU32); 22],
+}
+
+impl ObjectCount {
+    fn new() -> Self {
+        ObjectCount {
+            total: AtomicU32::new(0),
+            categories: [
+                ("object:property-change", AtomicU32::new(0)),
+                ("object:bounds-changed", AtomicU32::new(0)),
+                ("object:link-selected", AtomicU32::new(0)),
+                ("object:state-changed", AtomicU32::new(0)),
+                ("object:children-changed", AtomicU32::new(0)),
+                ("object:visible-data-changed", AtomicU32::new(0)),
+                ("object:selection-changed", AtomicU32::new(0)),
+                ("object:model-changed", AtomicU32::new(0)),
+                ("object:active-descendant-changed", AtomicU32::new(0)),
+                ("object:announcement", AtomicU32::new(0)),
+                ("object:attributes-changed", AtomicU32::new(0)),
+                ("object:row-inserted", AtomicU32::new(0)),
+                ("object:row-reordered", AtomicU32::new(0)),
+                ("object:row-deleted", AtomicU32::new(0)),
+                ("object:column-inserted", AtomicU32::new(0)),
+                ("object:column-reordered", AtomicU32::new(0)),
+                ("object:column-deleted", AtomicU32::new(0)),
+                ("object:text-bounds-changed", AtomicU32::new(0)),
+                ("object:text-selection-changed", AtomicU32::new(0)),
+                ("object:text-changed", AtomicU32::new(0)),
+                ("object:text-attributes-changed", AtomicU32::new(0)),
+                ("object:text-caret-moved", AtomicU32::new(0)),
+            ],
+        }
+    }
+
+    /// Increment the count for the given category
+    fn increment(&self, category: &'static str) {
+        self.total
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+
+        for (cat, count) in &self.categories {
+            if *cat == category {
+                count.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                return;
+            }
+        }
+    }
+
+    /// Get the total count
+    fn total(&self) -> u32 {
+        self.total.load(std::sync::atomic::Ordering::Relaxed)
+    }
+
+    /// Pretty print the stats
+    /// Order by percentage of total    
+    fn pretty_print_stats(&self) {
+        println!("\n\nTotal object events: {}", self.total());
+        let mut stats: Vec<(&'static str, u32)> = self
+            .categories
+            .iter()
+            .map(|(cat, count)| (*cat, count.load(std::sync::atomic::Ordering::Relaxed)))
+            .collect();
+        stats.sort_by(|a, b| b.1.cmp(&a.1));
+
+        for (cat, count) in stats {
+            let percentage = (count as f32 / self.total() as f32) * 100.0;
+            println!("{}: {} ({}%)", cat, count, percentage);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let atspi = atspi::AccessibilityConnection::new().await?;
@@ -86,12 +158,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let events = atspi.event_stream();
     tokio::pin!(events);
 
-    let count = Arc::new(InterfaceCount::new());
-    let ctrlc_count = count.clone();
+    let iface_count = Arc::new(InterfaceCount::new());
+    let obj_count = Arc::new(ObjectCount::new());
+    let ctrlc_iface_count = iface_count.clone();
+    let ctrl_c_obj_count = obj_count.clone();
 
     ctrlc::set_handler(move || {
         println!("\n\nStats:");
-        ctrlc_count.pretty_print_stats();
+        ctrlc_iface_count.pretty_print_stats();
+        ctrl_c_obj_count.pretty_print_stats();
         std::process::exit(0);
     })
     .expect("Error setting Ctrl-C handler");
@@ -100,16 +175,67 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     while let Some(Ok(ev)) = events.next().await {
         match ev {
-            Event::Object(_) => count.increment("object"),
-            Event::Window(_) => count.increment("window"),
-            Event::Document(_) => count.increment("document"),
-            Event::Terminal(_) => count.increment("terminal"),
-            Event::Mouse(_) => count.increment("mouse"),
-            Event::Keyboard(_) => count.increment("keyboard"),
-            Event::Listener(_) => count.increment("listener"),
-            Event::Cache(_) => count.increment("cache"),
-            Event::Focus(_) => count.increment("focus"),
-            Event::Available(_) => count.increment("available"),
+            Event::Object(objev) => {
+                iface_count.increment("object");
+                match objev {
+                    ObjectEvents::PropertyChange(_) => {
+                        obj_count.increment("object:property-change")
+                    }
+                    ObjectEvents::BoundsChanged(_) => obj_count.increment("object:bounds-changed"),
+                    ObjectEvents::LinkSelected(_) => obj_count.increment("object:link-selected"),
+                    ObjectEvents::StateChanged(_) => obj_count.increment("object:state-changed"),
+                    ObjectEvents::ChildrenChanged(_) => {
+                        obj_count.increment("object:children-changed")
+                    }
+                    ObjectEvents::VisibleDataChanged(_) => {
+                        obj_count.increment("object:visible-data-changed")
+                    }
+                    ObjectEvents::SelectionChanged(_) => {
+                        obj_count.increment("object:selection-changed")
+                    }
+                    ObjectEvents::ModelChanged(_) => obj_count.increment("object:model-changed"),
+                    ObjectEvents::ActiveDescendantChanged(_) => {
+                        obj_count.increment("object:active-descendant-changed")
+                    }
+                    ObjectEvents::Announcement(_) => obj_count.increment("object:announcement"),
+                    ObjectEvents::AttributesChanged(_) => {
+                        obj_count.increment("object:attributes-changed")
+                    }
+                    ObjectEvents::RowInserted(_) => obj_count.increment("object:row-inserted"),
+                    ObjectEvents::RowReordered(_) => obj_count.increment("object:row-reordered"),
+                    ObjectEvents::RowDeleted(_) => obj_count.increment("object:row-deleted"),
+                    ObjectEvents::ColumnInserted(_) => {
+                        obj_count.increment("object:column-inserted")
+                    }
+                    ObjectEvents::ColumnReordered(_) => {
+                        obj_count.increment("object:column-reordered")
+                    }
+                    ObjectEvents::ColumnDeleted(_) => obj_count.increment("object:column-deleted"),
+                    ObjectEvents::TextBoundsChanged(_) => {
+                        obj_count.increment("object:text-bounds-changed")
+                    }
+                    ObjectEvents::TextSelectionChanged(_) => {
+                        obj_count.increment("object:text-selection-changed")
+                    }
+                    ObjectEvents::TextChanged(_) => obj_count.increment("object:text-changed"),
+                    ObjectEvents::TextAttributesChanged(_) => {
+                        obj_count.increment("object:text-attributes-changed")
+                    }
+                    ObjectEvents::TextCaretMoved(_) => {
+                        obj_count.increment("object:text-caret-moved")
+                    }
+                }
+            }
+
+            Event::Window(_) => iface_count.increment("window"),
+            Event::Document(_) => iface_count.increment("document"),
+            Event::Terminal(_) => iface_count.increment("terminal"),
+            Event::Mouse(_) => iface_count.increment("mouse"),
+            Event::Keyboard(_) => iface_count.increment("keyboard"),
+            Event::Listener(_) => iface_count.increment("listener"),
+            Event::Cache(_) => iface_count.increment("cache"),
+            Event::Focus(_) => iface_count.increment("focus"),
+            Event::Available(_) => iface_count.increment("available"),
             _ => {}
         }
     }
